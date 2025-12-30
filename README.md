9e739b732c314864c4867ba898e7d3767b2e50bd
meprkbd <meprkbd@gmail.com>
2025-12-30 20:19:16 +0600

    chore: setup next.js app

---
+++ README.md
@@ -0,0 +1,3 @@
+# NexenSkool
+
+NexenSkool is an education-focused technology platform that provides software development services, academic and research guidance, professional training, and consultancy support. It aims to bridge the gap between education and practical technology skills through learning resources, project support, and technical solutions.
---
+++ src/app/layout.js
@@ -0,0 +1,21 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "NexenSkool",
+  description:
+    "NexenSkool is an education-focused technology platform that provides software development services, academic and research guidance, professional training, and consultancy support. It aims to bridge the gap between education and practical technology skills through learning resources, project support, and technical solutions.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={`${inter.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}
